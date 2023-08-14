03bab48cad6ab1953952d9d95325476347ca9094
kangel <277639786@qq.com>
2023-08-14 15:48:39 +0800

    why it keep crashing grr

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,4 +1,4 @@
-package;
+package backend;
 
 #if android
 import android.Tools;
@@ -55,12 +55,28 @@ class SUtil
 			if (!FileSystem.exists(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file')))
 				FileSystem.createDirectory(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file'));
 
-			if (!FileSystem.exists(SUtil.getPath() + 'assets'))
+			if (!FileSystem.exists(SUtil.getPath() + 'assets') && !FileSystem.exists(SUtil.getPath() + 'mods'))
 			{
 				SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the files from the .APK!\nPlease watch the tutorial by pressing OK.");
+				CoolUtil.browserLoad('https://b23.tv/qnuSteM');
 				System.exit(0);
 			}
-		
+			else
+			{
+				if (!FileSystem.exists(SUtil.getPath() + 'assets'))
+				{
+					SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the assets/assets folder from the .APK!\nPlease watch the tutorial by pressing OK.");
+					CoolUtil.browserLoad('https://b23.tv/qnuSteM');
+					System.exit(0);
+				}
+
+				if (!FileSystem.exists(SUtil.getPath() + 'mods'))
+				{
+					SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the assets/mods folder from the .APK!\nPlease watch the tutorial by pressing OK.");
+					CoolUtil.browserLoad('https://b23.tv/qnuSteM');
+					System.exit(0);
+				}
+			}
 		}
 		#end
 	}
